0dc61b6e70597af660f4cd8187afb00a7086987f
OverratedGman <k00691@reading-school.co.uk>
2015-09-13 10:51:43 +0100

    trying to fix some errors

--- app/src/main/java/com/example/gautham/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/gautham/popularmovies/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new MainActivityFragment()).commit();
+        }
     }
 
         @Override
